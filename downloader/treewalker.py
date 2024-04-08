62ec1aa56fb4bd8b777539c7045c04cf4bf58b1a
Jürgen Dufner <juergen@jdufner.de>
2024-04-08 09:39:24 +0200

    Status: open page, click consent and extract links

--- downloader/treewalker.py
+++ downloader/treewalker.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 import time
 
 from selenium import webdriver
@@ -6,7 +7,9 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.common.exceptions import TimeoutException
+from bs4 import BeautifulSoup
 
 
 class Treewalker:
@@ -20,83 +23,75 @@ class Treewalker:
         self.browser.get(url)
         timeout: int = 3  # seconds
         time.sleep(timeout)
-        html_source_code = self.browser.execute_script("return document.body.innerHTML;")
-        time.sleep(timeout)
-        html_source_code = self.browser.execute_script("return document.body.innerHTML;")
-
-        links = self.browser.find_elements(By.CSS_SELECTOR, 'button.message-button')  # type: list[WebElement]
-        for link in links:
-            logging.debug(f'link innerHTML = {link.get_attribute("innerHTML")}')
-
-        try:
-            logging.debug(f'Try to get an <button class="message-button" title="Zustimmen"> element')
-            web_element = WebDriverWait(self.browser, 3.0).until(EC.presence_of_element_located(
-                # (By.CSS_SELECTOR, 'button.message-button')))
-                (By.XPATH, '//button[@title="Zustimmen"]')))
-            logging.debug(f'element {web_element.get_attribute("innerHTML")}')
-        except TimeoutException:
-            logging.debug('Timeout of EC.presence_of_element_located')
-        # finally:
-
-        # try:
-        #     logging.debug(f'Try to get an <a> element')
-        #     web_element = WebDriverWait(self.browser, 10.0).until(EC.presence_of_element_located(
-        #         (By.TAG_NAME, 'a')))
-        #     logging.debug(f'element {web_element}')
-        # except TimeoutException:
-        #     logging.debug('Timeout of EC.presence_of_element_located')
-        # finally:
-
-        # try:
-        #     logging.debug(f'Try to get an <a>nach oben</a> element')
-        #     web_element = WebDriverWait(self.browser, 10.0).until(EC.text_to_be_present_in_element(
-        #         (By.TAG_NAME, 'a'), 'nach oben'))
-        #     logging.debug(f'element {web_element}')
-        # except TimeoutException:
-        #     logging.debug('Timeout of EC.text_to_be_present_in_element')
-        # finally:
-
-        # try:
-        #     logging.debug(f'Try to get an <a href="#top"> element')
-        #     web_element = WebDriverWait(self.browser, 10.0).until(EC.text_to_be_present_in_element_attribute(
-        #         (By.TAG_NAME, 'a'), 'href', '#top'))
-        #     logging.debug(f'element {web_element}')
-        # except TimeoutException:
-        #     logging.debug('Timeout of EC.text_to_be_present_in_element_attribute')
-        # finally:
 
         loaded: bool = False
         cookies_consented: bool = False
         for _ in range(10):
-            # html_soup: BeautifulSoup = BeautifulSoup(html_source_code, 'html.parser')
             if not cookies_consented:
                 cookies_consented = self.consent_cookies()
             if not loaded:
                 loaded = self.loaded()
 
+        self.find_links()
+
+        time.sleep(timeout)
         self.browser.quit()
+
+    def find_links(self) -> None:
+        html_source_code = self.browser.execute_script("return document.body.innerHTML;")
+        soup: BeautifulSoup = BeautifulSoup(html_source_code, 'html.parser')
+        links = soup.find_all('a')
+        links_len: int = len(links)
+        index: int = 1
+        for link in links:
+            logging.debug(f'link ({index}/{links_len}) href = {link.get('href')}')
+
+        # links = self.browser.find_elements(By.XPATH, '//a[@href]')
+        # links_len: int = len(links)
+        # logging.debug(f'links number = {links_len}')
+        # index: int = 1
+        # for link in links:
+        #     try:
+        #         logging.debug(f'link ({index}/{links_len}), innerHTML = {link.get_attribute('innerHTML')}, '
+        #                       f'href = {link.get_attribute('href')}')
+        #     except StaleElementReferenceException:
+        #         logging.error(f'Element {link.id} not found!')
+        #     except TimeoutException:
+        #         logging.error(f'Element {link.id} not found!')
+        #     finally:
+        #         index += 1
 
     def loaded(self) -> bool:
         timeout: int = 1
         try:
+            self.browser.switch_to.default_content()
             element_present = EC.presence_of_element_located(
                 (By.CSS_SELECTOR, 'html body div.container div footer.text-center a'))
             web_element: WebElement = WebDriverWait(self.browser, timeout).until(element_present)
-            logging.info(f'Page is ready! web_element {web_element.get_attribute("innerHTML")} found!')
+            logging.info(f'Web_element {web_element.get_attribute("innerHTML")} present!')
             return True
         except TimeoutException:
             logging.info("Loading took too much time!")
             return False
 
     def consent_cookies(self) -> bool:
-        timeout: int = 1
         try:
-            element_visible = EC.visibility_of_element_located(
-                (By.CSS_SELECTOR, 'button[title="Zustimmen"]'))
-            web_element: WebElement = WebDriverWait(self.browser, timeout).until(element_visible)
-            # web_element.click()
-            logging.info(f'Consented cookies! web_element {web_element.get_attribute("innerHTML")} found!')
+            self.browser.switch_to.frame(self.browser.switch_to.active_element)
+            logging.debug(f'browser title = {self.browser.title}')
+            # self.browser.get_screenshot_as_file(f'./logs/pics/screenshot_{datetime.now(): %Y-%m-%d_%Hh%Mm%Ss}.png')
+            buttons = self.browser.find_elements(By.CSS_SELECTOR, 'button[title="Zustimmen"]')  # type: list[WebElement]
+            for button in buttons:
+                inner_html: str = button.get_attribute("innerHTML")
+                logging.debug(f'button id = {button.id}, '
+                              f'innerHTML = {inner_html}, '
+                              f'title = {button.get_attribute('title')}, displayed = {button.is_displayed()}, '
+                              f'enabled = {button.is_enabled()}')
+                if button.is_displayed():
+                    button.click()
+                    logging.info(f'Button {inner_html} clicked!')
             return True
         except TimeoutException:
             logging.info("No cookie consent found!")
             return False
+        finally:
+            self.browser.switch_to.default_content()
