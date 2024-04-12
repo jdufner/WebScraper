3fb8c5fd7dc4d5a2a1b0fac09a971c33c1dbfe3b
Jürgen Dufner <juergen@jdufner.de>
2024-04-12 22:56:13 +0200

    Repository completed without any SQL framework

--- webscraper/repository.py
+++ webscraper/repository.py
@@ -1,5 +1,6 @@
-from _datetime import datetime
+from datetime import datetime
 import sqlite3
+from webscraper.document import Document
 
 
 class Repository:
@@ -11,7 +12,6 @@ class Repository:
         self.con.close()
 
     def create_tables(self):
-        now = datetime.now()
         self.cursor.execute('''CREATE TABLE documents (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             url VARCHAR(1000) NOT NULL,
@@ -20,14 +20,65 @@ class Repository:
             created_at DATETIME,
             created_by VARCHAR(100)
         )''')
-        self.cursor.executemany('INSERT INTO documents (url, content, downloaded_at) VALUES '
-                                '(:url, :content, :downloaded_at)', (
-                                    {'url': 'http://www.heise.de/', 'content': '<html></html>', 'downloaded_at': now},
-                                    {'url': 'http://www.heise.de/ct', 'content': '<html></html>', 'downloaded_at': now}
-                                ))
-        cur = self.cursor.execute('SELECT * from documents')
+        self.cursor.execute('''CREATE TABLE links (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            document_id INTEGER NOT NULL,
+            url VARCHAR(1000) NOT NULL
+        )''')
+        self.cursor.execute('''CREATE TABLE images (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            document_id INTEGER NOT NULL,
+            url VARCHAR(1000) NOT NULL,
+            filename VARCHAR(1000),
+            size INTEGER,
+            width INTEGER,
+            height INTEGER
+        )''')
+        # now = datetime.now()
+        # self.cursor.executemany('INSERT INTO documents (url, content, downloaded_at) VALUES '
+        #                         '(:url, :content, :downloaded_at)', (
+        #                             {'url': 'http://www.heise.de/', 'content': '<html></html>', 'downloaded_at': now},
+        #                             {'url': 'http://www.heise.de/ct', 'content': '<html></html>', 'downloaded_at': now}
+        #                         ))
+        # cur = self.cursor.execute('SELECT * from documents')
+        # print(cur.fetchall())
+
+    def save_document(self, document: Document):
+        self.cursor.execute('INSERT INTO documents (url, content, downloaded_at, created_at, created_by) VALUES'
+                            '(?, ?, ?, ?, ?)',
+                            (document.url, document.content, document.downloaded_at, document.created_ad[0],
+                             document.creators[0]))
+        document_id: int = self.cursor.lastrowid
+        self.cursor.executemany('INSERT INTO links (document_id, url) VALUES (:document_id, :url)',
+                                self.__list_to_link_dict(document, document_id))
+        self.cursor.executemany('INSERT INTO images (document_id, url) VALUES (:document_id, :url)',
+                                self.__list_to_link_dict(document, document_id))
+
+    def get_all(self):
+        cur = self.cursor.execute('SELECT * FROM documents d, links l, images i WHERE d.id = l.document_id and d.id = '
+                                  'i.document_id')
         print(cur.fetchall())
+
+    @staticmethod
+    def __list_to_link_dict(document: Document, document_id: int) -> list[dict]:
+        list_of_dicts: list[dict] = []
+        for link in document.links:
+            list_of_dicts.append({'document_id': document_id, 'url': link})
+        return list_of_dicts
+
+    @staticmethod
+    def __document_to_images_dict(document: Document, document_id: int) -> list[dict]:
+        list_of_dicts: list[dict] = []
+        for image_url in document.image_urls:
+            list_of_dicts.append({'document_id': document_id, 'url': image_url})
+        return list_of_dicts
 
 
 if __name__ == '__main__':
-    Repository().create_tables()
+    r = Repository()
+    r.create_tables()
+    d = Document('https://www.heise.de/', '<html>..</html>', datetime.now(),
+                 [str(datetime.now())], ['Michael Schmitt'], ['https://www.heise.de/ct', 'https://www.heise.de/ix'],
+                 ['https://www.heise.de/pic.jpg'])
+    r.save_document(d)
+    r.get_all()
